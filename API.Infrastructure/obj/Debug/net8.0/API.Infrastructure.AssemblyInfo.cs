6011be5a82d3a39ce736191fd089808eca179865
DeeprajBorse <borsedeepraj77@gmail.com>
2026-09-16 17:25:37 +0530

    Test Errors Updated

--- API.Infrastructure/obj/Debug/net8.0/API.Infrastructure.AssemblyInfo.cs
+++ API.Infrastructure/obj/Debug/net8.0/API.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c289a0274d37fbc0ce3d45eeac43a687e071c1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02ef2bc1ba00c8a9f5420c33399f267a57c591f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
