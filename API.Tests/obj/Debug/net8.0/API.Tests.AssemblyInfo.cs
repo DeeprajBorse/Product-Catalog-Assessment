1c3eb6cd70981e87df584f9a84f45ffc580b60db
DeeprajBorse <borsedeepraj77@gmail.com>
2026-09-16 17:49:45 +0530

    Final Commit

--- API.Tests/obj/Debug/net8.0/API.Tests.AssemblyInfo.cs
+++ API.Tests/obj/Debug/net8.0/API.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6011be5a82d3a39ce736191fd089808eca179865")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a35018b51205ac842181f89f9c846d901c7b4d00")]
 [assembly: System.Reflection.AssemblyProductAttribute("API.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
